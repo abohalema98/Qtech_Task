632590c4aa5882d3583f0fe94af20e7a97d9b5b3
Mohamed Ahmed <eng.mohameda7med@gmail.com>
2022-02-20 01:59:58 +0200

    add server Validation

--- backend/Validation/validations.js
+++ backend/Validation/validations.js
@@ -1,14 +1,23 @@
-const { body, validationResult } = require('express-validator');
+const joi = require("joi")
 
+//register validation before creating a user
+const registerValidation = data => {
+    const schema = joi.object({
+        UserName: joi.string().min(3).max(30).required(),
+        Email: joi.string().required().email(),
+        Password: joi.string().required().min(6).max(1024),
+    });
+    return schema.validate(data)
 
-const UserValidation = req => {
-    body("Email").isEmail(),
-        body("Password").isLength({ min: 6 })
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
 }
 
-
-module.exports = UserValidation;
+//login validation 
+const loginValidation = data => {
+    const schema = joi.object({
+        Email: joi.string().required().email().min(6).max(255),
+        Password: joi.string().required().min(6).max(12)
+    });
+    return schema.validate(data)
+}
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
